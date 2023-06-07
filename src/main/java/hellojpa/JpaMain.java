32956d27b8a88130dbfa5638395996bb7a3cb1ea
SH Kim <82145134+shkim-123@users.noreply.github.com>
2023-06-07 22:03:59 +0900

    영속성 컨텍스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -27,14 +27,41 @@ public class JpaMain {
 //            findMember.setName("HelloJPA");
 
             // JPQL
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(8)
-                    .getResultList();
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(1)
+//                    .setMaxResults(8)
+//                    .getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member.getName() = " + member.getName());
+//            }
 
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+            /*
+            // 비영속
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
+
+            // 영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
+
+            // 1차 캐시에서 조회
+            Member findMember = em.find(Member.class, 101L);
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
+             */
+
+            /*
+            // 영속
+            Member findMember1 = em.find(Member.class, 101L); // DB 조회 후 1차 캐시에 저장
+            Member findMember2 = em.find(Member.class, 101L); // 1차 캐시에서 조회
+
+            System.out.println("result = " + (findMember1 == findMember2));
+             */
+
+
 
             tx.commit();
         } catch (Exception e) {
